a15a0c90cb672d7c53128cfab25c2aa7c87077f8
scary <imbamrgrey@gmail.com>
2019-03-11 17:15:35 +0100

    new test first

---
+++ src/test/scala/GooseCellsTests.scala
@@ -0,0 +1,30 @@
+
+import entities.{Player, Position}
+import infrastructure.{CommandParser, DataBase}
+import org.scalatest.Matchers._
+import org.scalatest.{FlatSpec, _}
+
+class WinsPlayersTests extends FlatSpec {
+  implicit val ctx: DataBase = new DataBase()
+  val pippo = Player("Pippo")
+
+  ctx.players += pippo
+  ctx.positions += Position(pippo, 60)
+
+  behavior of "If there is one participant \"Pippo\" on space \"60\" the system"
+
+  val steps = Array(
+    ("move Pippo 1, 2", "Pippo rolls 1, 2. Pippo moves from 60 to 63. Pippo Wins!!")
+  )
+
+  for (step <- steps) {
+    val commandText = step._2.toString
+    it should s"responds: $commandText  if the user writes: ${step._1}" in {
+      val command = CommandParser getCommandFrom step._1
+      command.execute should equal(Right(commandText))
+
+      ctx.positions should contain only Position(pippo, 63)
+    }
+  }
+
+}

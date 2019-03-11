93fd81152d4da6e614d96b7b85a389ab9d975014
scary <imbamrgrey@gmail.com>
2019-03-11 17:57:50 +0100

    test fix

--- src/main/scala/entities/Position.scala
+++ src/main/scala/entities/Position.scala
@@ -9,21 +9,22 @@ sealed abstract class BasePosition {
   override def toString: String = translate(cell)
 
   override def equals(obj: Any): Boolean = obj match {
-    case bp : BasePosition => this.player.name == bp.player.name
+    case bp: BasePosition => this.player.name == bp.player.name
     case _ => super.equals(obj)
   }
 
   def moveOn(n: Int): (BasePosition, String) = {
     val text = s"$player moves from ${translate(cell)} to ${translate(cell + n)}"
 
-    def moveOnInner(n: Int) = cell + n match {
-      case 63 => (WinPosition(player, 63), text.concat(s". $player Wins!!"))
-      case a if a > 63 => (Position(player, 63), text)
-      case 6 => (Position(player, 12), text.concat(s". $player jumps to 12"))
-      case x => (Position(player, x), text)
+    def moveOnInner(prev: Int, n: Int, innerText: String): (BasePosition, String) = prev + n match {
+      case 63 => (WinPosition(player, 63), innerText.concat(s". $player Wins!!"))
+      case a if a > 63 => (Position(player, 63), innerText)
+      case 6 => (Position(player, 12), innerText.concat(s". $player jumps to 12"))
+      case sum@(5 | 9 | 14 | 18 | 23 | 27) => moveOnInner(sum, n, innerText.concat(s", The Goose. $player moves again and goes to ${sum + n}"))
+      case x => (Position(player, x), innerText)
     }
 
-    moveOnInner(n)
+    moveOnInner(cell, n, text)
   }
 }
 
--- src/test/scala/GooseCellsTests.scala
+++ src/test/scala/GooseCellsTests.scala
@@ -6,45 +6,46 @@ import org.scalatest.Matchers._
 
 
 class GooseCellsTests extends FlatSpec {
+
   implicit val ctx: DataBase = new DataBase()
   val pippo = Player("Pippo")
 
-  ctx.players += pippo
-  ctx.positions += Position(pippo, 3)
 
   behavior of "If there is one participant \"Pippo\" on space \"3\" " +
     "assuming that the dice get 1 and 1 " +
     "the system"
 
-  val steps = Array(
-    ("move Pippo 1, 1", "Pippo rolls 1, 1. Pippo moves from 3 to 5, The Goose. Pippo moves again and goes to 7")
-  )
+  {
+    implicit val ctx: DataBase = new DataBase()
+    ctx.players += pippo
+    ctx.positions += Position(pippo, 3)
+    val steps = ("move Pippo 1, 1", "Pippo rolls 1, 1. Pippo moves from 3 to 5, The Goose. Pippo moves again and goes to 7")
+    val responseText = steps._2.toString
+    it should s"responds: $responseText  if the user writes: ${steps._1}" in {
+      val command = CommandParser getCommandFrom steps._1
+      command.execute should equal(Left(responseText))
 
-  for (step <- steps) {
-    val commandText = step._2.toString
-    it should s"responds: $commandText  if the user writes: ${step._1}" in {
-      val command = CommandParser getCommandFrom step._1
-      command.execute should equal(Right(commandText))
-
-      ctx.positions should contain only Position(pippo, 63)
+      ctx.positions should contain only Position(pippo, 7)
     }
   }
 
-  ctx.players += pippo
-  ctx.positions.clear()
-  ctx.positions += Position(pippo, 10)
-  behavior of "If there is one participant \"Pippo\" on space \"10\" " +
-    "assuming that the dice get 2 and 2 " +
-    "the system"
-  val testText =
-    ("move Pippo 2, 2", "Pippo rolls 2, 2. Pippo moves from 10 to 14, The Goose. Pippo moves again and goes to 18, The Goose. Pippo moves again and goes to 22Pippo rolls 1, 1. Pippo moves from 3 to 5, The Goose. Pippo moves again and goes to 7")
+  {
+    implicit val ctx: DataBase = new DataBase()
+    ctx.players += pippo
+    ctx.positions += Position(pippo, 10)
+
+    behavior of "If there is one participant \"Pippo\" on space \"10\" " +
+      "assuming that the dice get 2 and 2 " +
+      "the system"
+    val testText = ("move Pippo 2, 2", "Pippo rolls 2, 2. Pippo moves from 10 to 14, The Goose. Pippo moves again and goes to 18, The Goose. Pippo moves again and goes to 22")
 
     val commandText = testText._2.toString
     it should s"responds: $commandText  if the user writes: ${testText._1}" in {
       val command = CommandParser getCommandFrom testText._1
-      command.execute should equal(Right(commandText))
+      command.execute should equal(Left(commandText))
 
-      ctx.positions should contain only Position(pippo, 63)
+      ctx.positions should contain only Position(pippo, 22)
     }
+  }
 
 }
